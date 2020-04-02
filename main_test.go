cb450e8ef2f3fe3476517567d12aefec2817a2d6
thanawatgulati <thanawatgulatijames@gmail.com>
2020-04-03 00:42:43 +0700

    🏎 BenchMark TEST

--- main_test.go
+++ main_test.go
@@ -16,6 +16,8 @@ func Test_fib(t *testing.T) {
 		{name: "CASE 1", args: args{0}, want: 0},
 		{name: "CASE 2", args: args{1}, want: 1},
 		{name: "CASE 3", args: args{10}, want: 55},
+		{name: "CASE 4", args: args{11}, want: 89},
+		{name: "CASE 5", args: args{12}, want: 144},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -43,5 +45,17 @@ func Test_add(t *testing.T) {
 				t.Errorf("add() = %v, want %v", got, tt.want)
 			}
 		})
+	}
+}
+
+func Benchmark_Fibo25_1(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		fibSlow(28)
+	}
+}
+
+func Benchmark_Fibo25_2(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		fib(28)
 	}
 }
