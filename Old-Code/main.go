cf6396ebdc0e75c0084ba3f6c86da98ebfdcac31
thanawatgulati <thanawatgulatijames@gmail.com>
2020-05-23 02:09:04 +0700

    😉 Renew

---
+++ Old-Code/main.go
@@ -0,0 +1,24 @@
+// package main
+
+// import (
+// 	"github.com/gin-gonic/gin"
+
+// 	"net/http"
+// )
+
+// func brokenHandler(c *gin.Context) {
+// 	c.JSON(http.StatusOK, gin.H{"name": "James"})
+// }
+
+// func helloHandler(c *gin.Context) {
+// 	c.JSON(http.StatusOK, gin.H{
+// 		"message": "Hello Gin🍹",
+// 	})
+// }
+
+// func main() {
+// 	r := gin.Default()
+// 	r.GET("/", helloHandler)
+// 	r.GET("/broken", brokenHandler)
+// 	r.Run()
+// }
--- main.go
+++ main.go
@@ -1,24 +1,7 @@
 package main
 
-import (
-	"github.com/gin-gonic/gin"
-
-	"net/http"
-)
-
-func brokenHandler(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{"name": "James"})
-}
-
-func helloHandler(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{
-		"message": "Hello Gin🍹",
-	})
-}
+import "fmt"
 
 func main() {
-	r := gin.Default()
-	r.GET("/", helloHandler)
-	r.GET("/broken", brokenHandler)
-	r.Run()
+	fmt.Println("Hello World")
 }
