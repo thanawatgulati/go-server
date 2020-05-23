557ecb498ef91ab41a87e15ca519e5d69674a15c
thanawatgulati <thanawatgulatijames@gmail.com>
2020-05-23 11:36:05 +0700

    🍹 GIN ROUTE

--- main.go
+++ main.go
@@ -1,8 +1,13 @@
 package main
 
-import "fmt"
+import "github.com/gin-gonic/gin"
 
 func main() {
-	fmt.Println("Hello World")
-	fmt.Println("Hello World2")
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "Hello World",
+		})
+	})
+	r.Run(":9999")
 }
